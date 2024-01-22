d9a0cd1fee9897f08932a7802c9939956a260a6f
taner <tanerozer16@gmail.com>
2024-01-22 11:19:49 +0300

    User model added

---
+++ model/User.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  age: {
+    type: Number,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+  updated_at: {
+    type: Date,
+    default: null,
+  },
+});
+
+userSchema.pre("save", function (next) {
+  this.updated_at = new Date();
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
