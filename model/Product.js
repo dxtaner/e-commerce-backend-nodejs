5d81abe07f3377410cc4894e7ee5d5cc8056f537
taner <tanerozer16@gmail.com>
2024-01-22 11:19:32 +0300

    Product model added

---
+++ model/Product.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+productSchema.statics.getCollectionName = function () {
+  return "products";
+};
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
