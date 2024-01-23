d7cc24a10d583adca9b878fd8da750fb7f74d7cd
taner <tanerozer16@gmail.com>
2024-01-23 11:51:18 +0300

    MainRouter.js added

---
+++ routers/MainRouter.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+
+const productRouter = require("./ProductRouter.js");
+const authRouter = require("./AuthRouter.js");
+const userRouter = require("./UserRouter.js");
+const cartItemRouter = require("./CartItemRouter.js");
+const fileRouter = require("./FileRouter.js");
+const searchRouter = require("./SearchRouter.js");
+
+const AuthValidator = require("../validators/AuthValidator.js");
+
+router.use("/product", AuthValidator.authCheck, productRouter);
+router.use("/auth", authRouter);
+router.use("/user", AuthValidator.authCheck, userRouter);
+router.use("/cartitem", AuthValidator.authCheck, cartItemRouter);
+router.use("/file", AuthValidator.authCheck, fileRouter);
+router.use("/search", searchRouter);
+
+module.exports = router;
