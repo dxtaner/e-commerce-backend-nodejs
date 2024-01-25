ed830e802d496852cb650355d107a7fd0405e7c4
taner <tanerozer16@gmail.com>
2024-01-25 12:13:32 +0300

    UserValidator added

---
+++ validators/UserValidator.js
@@ -0,0 +1,52 @@
+// validators/UserValidator.js
+const { body, param, query, validationResult } = require("express-validator");
+const User = require("../model/User");
+
+const loginValidator = [
+  body("email").isEmail().withMessage("Invalid email address"),
+  body("password").notEmpty().withMessage("Password is required"),
+];
+
+const registerValidator = [
+  body("name").notEmpty().withMessage("Name is required"),
+  body("email")
+    .isEmail()
+    .withMessage("Invalid email address")
+    .custom(async (value) => {
+      const existingUser = await User.findOne({ email: value });
+      if (existingUser) {
+        return Promise.reject("Email is already in use");
+      }
+      return true;
+    }),
+  body("age").optional({ nullable: true }).isInt().withMessage("Invalid age"),
+  body("password").notEmpty().withMessage("Password is required"),
+];
+
+const getUserValidator = [
+  param("objectId").isMongoId().withMessage("Invalid objectId"),
+];
+
+const getAllUsersValidator = [
+  query("page").optional().isInt({ min: 1 }).withMessage("Invalid page number"),
+  query("limit")
+    .optional()
+    .isInt({ min: 1, max: 100 })
+    .withMessage("Invalid limit value"),
+];
+
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    return next();
+  }
+
+  return res.status(400).json({ errors: errors.array() });
+};
+module.exports = {
+  loginValidator,
+  registerValidator,
+  validate,
+  getUserValidator,
+  getAllUsersValidator,
+};
